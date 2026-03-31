523cb9c0c33afcb8056d08af3ecea600238ca276
wcy142857 <18819057524@163.com>
2026-03-31 15:38:34 +0800

    Add files via upload

---
+++ custom-robot-group-message-quick-start-java/src/main/java/customRobotGroupMessage.java
@@ -0,0 +1,64 @@
+import com.dingtalk.api.DefaultDingTalkClient;
+import com.dingtalk.api.DingTalkClient;
+import com.dingtalk.api.request.OapiRobotSendRequest;
+import com.dingtalk.api.response.OapiRobotSendResponse;
+import com.taobao.api.ApiException;
+import org.apache.commons.codec.binary.Base64;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+public class customRobotGroupMessage {
+
+    public static final String CUSTOM_ROBOT_TOKEN = "3d567188e4b6494246ba91227138c83268b2b655189f4f6354878b5c1263b033";
+
+    public static final String USER_ID= "Scarlett Sui";
+
+    public static final String SECRET = "<your custom robot SECRET>";
+
+    public static void main(String[] args) {
+        try {
+            Long timestamp = System.currentTimeMillis();
+            System.out.println(timestamp);
+            String secret = SECRET;
+            String stringToSign = timestamp + "\n" + secret;
+            Mac mac = Mac.getInstance("HmacSHA256");
+            mac.init(new SecretKeySpec(secret.getBytes("UTF-8"), "HmacSHA256"));
+            byte[] signData = mac.doFinal(stringToSign.getBytes("UTF-8"));
+            String sign = URLEncoder.encode(new String(Base64.encodeBase64(signData)),"UTF-8");
+            System.out.println(sign);
+
+            //sign字段和timestamp字段必须拼接到请求URL上，否则会出现 310000 的错误信息
+            DingTalkClient client = new DefaultDingTalkClient("https://oapi.dingtalk.com/robot/send?sign="+sign+"&timestamp="+timestamp);
+            OapiRobotSendRequest req = new OapiRobotSendRequest();
+            /**
+             * 发送文本消息
+             */
+            //定义文本内容
+            OapiRobotSendRequest.Text text = new OapiRobotSendRequest.Text();
+            text.setContent("我是扫福瑞");
+            //定义 @ 对象
+            OapiRobotSendRequest.At at = new OapiRobotSendRequest.At();
+            at.setAtUserIds(Arrays.asList(USER_ID));
+            //设置消息类型
+            req.setMsgtype("text");
+            req.setText(text);
+            req.setAt(at);
+            OapiRobotSendResponse rsp = client.execute(req, CUSTOM_ROBOT_TOKEN);
+            System.out.println(rsp.getBody());
+        } catch (ApiException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        } catch (InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
